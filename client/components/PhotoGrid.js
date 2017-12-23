214d0bb7e85eaf4bf13cc7bde565c222e48c6a7d
charan1922 <charaneie22@gmail.com>
2017-12-23 11:19:22 +0530

    added client

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class PhotoGrid extends Component {
+  render() {
+    return (
+      <div className="photo-grid">
+      I'm PhotoGrid
+        
+      </div>
+    )
+  }
+}
