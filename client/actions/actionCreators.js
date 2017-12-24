668e24c182e8b0b6691d6d26562a258ff2f47e49
charan1922 <charaneie22@gmail.com>
2017-12-24 23:30:44 +0530

    updated action creators

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,24 @@
+const increment = index => {
+    return {
+        type: 'INCREMENT_LIKES',
+        index
+    }
+},
+
+addComment = (postId , author , comment) => {
+    return {
+        type:'ADD_INCREMENT',
+        postId,
+        author,
+        comment
+    }
+},
+removeComment = (postId , i) =>{
+    return {
+        type:'REMOVE_COMMENT',
+        postId,
+        i
+    }
+};
+
+export default (increment,addComment,removeComment);
