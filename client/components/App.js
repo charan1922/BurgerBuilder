2be9474725c7e315fbb7a8edf4153f8aaf511c79
charan1922 <charaneie22@gmail.com>
2017-12-24 23:29:41 +0530

    Added App reducer

---
+++ client/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+//import Actions
+import * as ActionCreators from '../actions/actionCreators';
+
+//import Main component
+import Main from './Main';
+
+
+function mapStateToProps(state){
+    return {
+        posts:state.posts,
+        comments: state.comments
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    console.log('j')
+    return bindActionCreators(ActionCreators, dispatch)
+}
+
+
+
+const App = connect(mapStateToProps,mapDispatchToProps)(Main);
+
+export default App;
