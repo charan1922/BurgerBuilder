35ceb242bec2797ed1629947b3c7e072c92c2755
charan1922 <charaneie22@gmail.com>
2017-12-24 12:30:45 +0530

    Updated application

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
- import { Link } from 'react-router';
+import React, { Component } from "react";
+import { Link } from "react-router";
 
- class Main extends Component {
+class Main extends Component {
   render() {
-    return (
-      <div>  
-          <h1>
-        <Link to='/'>Reduxtragram</Link>
-     </h1>
+    return 
+    (
+      <div>
+        <h1>
+          <Link to="/"> Reduxtragram </Link>
+         {/* { React.cloneElement(this.props.children, this.props) } */}
+        </h1>
       </div>
-    )
+    );
   }
 }
 
-export default Main; 
+export default Main;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,6 +2,14 @@
 import React from 'react';
 import {render} from 'react-dom'
 import css from './styles/style.styl';
+
+
 import Main from './components/Main';
 
-render(<Main/>,document.getElementById('root'));
+
+const router();
+
+
+render(
+router
+,document.getElementById('root'));
